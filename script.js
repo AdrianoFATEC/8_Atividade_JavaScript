9bd61a6facdc6b5ef5862b4c2120415287d6f47d
Adriano <adriano.bezerra4@fatec.sp.gov.br>
2023-10-02 15:25:54 -0300

    Atividade JavaScript. I 08

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let titulo = document.createElement("h1");
-titulo.textContent = "Meu site";
+
 titulo.id = "titulo";
-document.body.appendChild(titulo);
+titulo.innerText = "Loja de Jogos de Playstation";
+
+let body = document.querySelector("body");
+
+body.appendChild(titulo);
+
+
+let produto = document.createElement("div");
+
+produto.innerHTML = `
+  <div>
+    <h2>Final Fantasy VII</h2>
+    <img src="https://t2.tudocdn.net/603695?w=1920" alt="FinalFantasy">
+    <p>Jogo de RPG</p>
+    <p id="preco-FinalFantasy">R$ 150.00</p>
+  </div>
+`
+
+
+body.appendChild(produto)
