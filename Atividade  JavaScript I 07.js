f7fe19c289f13eef6bc410d05f1cce791f9a715f
Adriano <adriano.bezerra4@fatec.sp.gov.br>
2023-10-02 14:04:39 -0300

    Atividade JavaScript I 07

---
+++ Atividade  JavaScript I 07.js
@@ -0,0 +1,16 @@
+function calcularOperacoes(num1, num2) {
+  var soma = num1 + num2;
+  console.log("soma:", num1 + " + " + num2 + " = " + soma);
+
+  var subtracao = num1 - num2;
+  console.log("Subtração:", num1 + " - " + num2 + " = " + subtracao);
+
+  var multiplicacao = num1 * num2;
+  console.log("Multiplicação:", num1 + " x " + num2 + " = " + multiplicacao);
+
+  var divisao = num1 / num2;
+  console.log("Divisão:", num1 + " / " + num2 + " = " + divisao);
+}
+
+calcularOperacoes(4, 5);
+
